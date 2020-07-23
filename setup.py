a4e71bc6a11b31c82574c314d4bcb388700e8f43
TeddyBearSuicide <unconfigured@null.spigotmc.org>
2020-07-23 11:43:38 -0500

    updated gitignore, added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="unittrial",
+    version="0.1.0",
+    author="TeddyBearSuicide",
+    description="Python Test Framework",
+    url="https://github.com/TeddyBearSuicide/unittrial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: LGPL License",
+        "Operating System :: OS Independent"
+    ]
+)
+
